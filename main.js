7d7105fd5269136e409c904087c878099684adb7
Hamza Khan Lodhi <134397582+hamzalodhi2023@users.noreply.github.com>
2024-01-18 16:17:37 +0500

    complete

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 let km = document.querySelector("#km");
 let miles = document.querySelector("#miles");
 let btn = document.querySelector("#btn");
+let clear = document.querySelector("#clear");
+let body = document.querySelector("body");
+const factor = 0.621371;
 
-btn.addEventListener("input", function () {});
+btn.addEventListener("click", function () {
+  miles.value = km.value * factor;
+});
+
+body.addEventListener("load", function () {
+  miles.value = "";
+  km.value = "";
+});
+
+clear.addEventListener("click", function () {
+  miles.value = "";
+  km.value = "";
+});
